a23ff0b6db1aebb9eed81a1d02b52de75966b8c6
Pushpak <pushpakzambare333@gmail.com>
2025-05-20 12:05:07 +0530

    aws/devOps/azure

--- Script4.sh
+++ Script4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is creation and calling of shell variable --- defining variables
-Class=pushpak
-Batch=2528
-PROFESSION=AWS/DevSecOps/timeps DevOps
+Class=zambare
+Batch=28
+PROFESSION=AWS/DevSecOps/timeps class
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
